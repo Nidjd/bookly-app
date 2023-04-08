88c44e7695660b5f2431c8a36330f480af827824
Mohammed Almahmoud <malmhmwd238@gmail.com>
2023-04-08 12:11:54 -0700

    search view done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -3,6 +3,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../../../../core/utils/styles.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -15,8 +17,38 @@ class SearchViewBody extends StatelessWidget {
       child: Column(
         children: const [
           CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Result',
+            textAlign: TextAlign.start,
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(child: SearchResultListView()),
         ],
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
